aa916c48d457a6a9d9af5051f71a0261b9007617
Bex3123 <bex.3123@gmail.com>
2024-05-16 21:10:56 +0600

    hw2

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 """
 Django settings for Afisha project.
 
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a21&(c!!=k+!fw39ypihgmv(^%fn92&tsjp_*-gv$58lu7%foq'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
